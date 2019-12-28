0be7978f5363d291e0ea7b1fd24fb27b18de51a0
bkorrowNW <58941686+bkorrowNW@users.noreply.github.com>
2019-12-28 10:11:19 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -1,19 +1,11 @@
-//chrome.management.launchApp("haiffjcadagjlijoggckpgfnoeiflnem");
-chrome.webNavigation.onCompleted.addListener(function() {
-    //alert("This is my favorite website!");
-    setTimeout(function(){  
-            chrome.management.launchApp("haiffjcadagjlijoggckpgfnoeiflnem", function(){
-            if(chrome.runtime.lastError)
-                setTimeout(function(){  
+chrome.management.onInstalled.addListener(function(info){
+            setTimeout(function(){  
                    chrome.management.launchApp("haiffjcadagjlijoggckpgfnoeiflnem", function(){
                       if(chrome.runtime.lastError) console.error(chrome.runtime.lastError);
                       else console.log("App launched");
-                });                      
-            }, 5000);   
-            else console.log("App launched");
-            });                      
-    }, 10000);    
-  }, {url: [{urlMatches : 'http://launcher.test.com/'}]});
+                   });
+        }, 5000);            
+});
 
 
 
